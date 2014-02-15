7c963d7cf41b3e940bb57a6f7b657a6ab25c92cd
Jorge Puente Sarrín <puentesarrin@gmail.com>
2014-02-15 22:30:01 +0000

    Test coverage for handlers returning a value.

--- tests/handlers_test.py
+++ tests/handlers_test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from tornado.escape import utf8
+from tornado.testing import ExpectLog
 from bonzo import errors
 from bonzo.handlers import MessageHandler
 from bonzo.testing import AsyncSMTPTestCase
@@ -63,3 +65,32 @@ class HandlerErrorLogMessageTest(server_test.SMTPServerErrorLogMessageTest):
 
         return Handler(status_code=self.status_code, message=self.message,
                        log_message=self.log_message)
+
+
+class HandlerNotReturnsNoneTest(AsyncSMTPTestCase):
+
+    def get_request_callback(self):
+        class Handler(MessageHandler):
+
+            def data(self, request):
+                return True
+
+        return Handler()
+
+    def test_returns_true(self):
+        self.connect()
+        self.stream.write(b'MAIL FROM:mail@example.com\r\n')
+        data = self.read_response()
+        self.assertEqual(data, b'250 Ok\r\n')
+        for address in ['mail@example.com', '<mail@example.com>']:
+            self.stream.write(utf8('RCPT TO:%s\r\n' % address))
+            data = self.read_response()
+            self.assertEqual(data, b'250 Ok\r\n')
+        self.stream.write(b'DATA\r\n')
+        data = self.read_response()
+        self.assertEqual(data, b'354 End data with <CR><LF>.<CR><LF>\r\n')
+        with ExpectLog('tornado.application', 'Uncaught exception'):
+            self.stream.write(b'This is a message\r\n.\r\n')
+            data = self.read_response()
+            self.assertEqual(data, b'451 Internal confusion\r\n')
+        self.close()
